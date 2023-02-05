472c3b7ab6783a6ff53ed907ae061f15aee18beb
Zudikhin <uzudihin@gmail.com>
2023-02-06 01:46:25 +0300

    full

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 $(document).ready(function () {
     "use strict";
 
+    $(document).mouseup(function(e)  {
+        var container = $(".about_map_block_right_modal");
+        if (!container.is(e.target) && container.has(e.target).length === 0) {
+            $(".about_map_block_right_modal").removeClass("active");
+        }
+    });
+
     $(".nav_mobile_top_btn").click(function() {
         $(this).toggleClass("active");
         $(".nav_mobile_drop").toggleClass("active");
@@ -8,6 +15,12 @@ $(document).ready(function () {
 
     $(".blog_content_item_content_read_btn").click(function(){
         $(this).parent().parent().parent().parent().addClass("active");
+    });
+
+    $(".map_item").click(function() {
+        var id = $(this).attr('id');
+        $(".about_map_block_right_modal").removeClass("active");
+        $(`.about_map_block_right_modal[data-target='${id}']`).addClass("active");
     });
 
     $('.main_slider').slick({
@@ -100,21 +113,64 @@ $(document).ready(function () {
         arrows: false
       });
 
-    $('.supplier_range_slider').slick({
+    $('.supplier_range_slider_wrap').slick({
+        dots: false,
+        infinite: true,
+        arrows: true,
+        autoplaySpeed: 1,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: false,
+        cssEase: 'linear',
+        variableWidth: true,
+        prevArrow: $('.supplier_range_slider_left'),
+        nextArrow: $('.supplier_range_slider_right')
+    });
+
+    $('.foreign_suppliers_ribbon_slider').slick({
         dots: false,
         infinite: true,
         autoplay: true,
-        arrows: false,
-        autoplaySpeed: 0,
-        speed: 6000,
-        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 500,
+        autoplaySpeed: 1000,
         centerMode: false,
-        draggable:false,
         focusOnSelect:false,
         pauseOnFocus:false,
         pauseOnHover:false,
         cssEase: 'linear',
-        variableWidth: true
+        variableWidth: true,
+        prevArrow: $('.foreign_suppliers_ribbon_prev'),
+        nextArrow: $('.foreign_suppliers_ribbon_next'),
+    });
+
+    $('.supplier_logistics_feature_slider_wrap').slick({
+        dots: false,
+        infinite: true,
+        arrows: true,
+        variableWidth: false,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        speed: 500,
+        cssEase: 'linear',
+        prevArrow: $('.supplier_logistics_feature_slider_prev'),
+        nextArrow: $('.supplier_logistics_feature_slider_next'),
+        responsive: [
+            {
+              breakpoint: 991,
+              settings: {
+                rows: 2,
+                dots: false,
+                arrows: false,
+                infinite: true,
+                speed: 300,
+                slidesPerRow: 1,
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+        ]
     });
 
     $('input[type="tel"]').inputmask("+7(999)999-99-99");
